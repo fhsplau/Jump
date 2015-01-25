72fa1c9c6c8feba3058bf46fecbccdb788dc5f90
paverell <piotr.kacprzak11@gmail.com>
2015-01-25 20:34:09 +0000

    code reorganized

--- src/main/scala/io/jump/suite/Suite.scala
+++ src/main/scala/io/jump/suite/Suite.scala
@@ -19,30 +19,31 @@ trait ContentMatcher {
       case e: NoSuchElementException => None
     }
   }
+
+  def getTags(tagsType: String): List[String] = matchFields("@" + tagsType + " tags" + ":") match {
+    case Some(i) => i.split(",").toList.map(_.replace(" ", ""))
+    case None => List()
+  }
 }
 
-trait SuiteContent {
+//  Get or else
+abstract class Content extends ContentMatcher {
+
+  val name: String
+  val tags: List[String]
+
+}
+
+trait SuiteContent extends Content{
   protected val f: File
 
+  val doc: String
   val tests: List[Test]
 
   protected def getContent(path: String): List[String] = try {
     Source.fromFile(f).getLines().toList
   } catch {
     case e: FileNotFoundException => throw new Exception("[error] File not found")
-  }
-}
-//  Get or else
-abstract class Content extends ContentMatcher {
-
-  val name: String
-  val tags: List[String]
-  val doc: String
-
-
-  def getTags(tagsType: String): List[String] = matchFields("@" + tagsType + " tags" + ":") match {
-    case Some(i) => i.split(",").toList.map(_.replace(" ", ""))
-    case None => List()
   }
 
   def getDoc(docType: String): String = matchFields("@" + docType + ":") match {
@@ -51,25 +52,31 @@ abstract class Content extends ContentMatcher {
   }
 }
 
-case class Test(content: List[String]) extends Content {
-  override val name: String = ""
-  override val doc: String = ""
-  override val tags: List[String] = List()
+trait TestContent extends Content {
+  val steps: List[String]
 }
 
-case class Suite(private val path: String) extends Content with SuiteContent {
+case class Test(content: List[String]) extends TestContent {
+  override val name: String = ""
+  override val tags: List[String] = List()
+  override val steps: List[String] = List()
+}
+
+case class Suite(private val path: String) extends SuiteContent {
 
   override val f = new File(path)
 
-  override protected val content = getContent(path)
-
   override val name: String = f.getName
+
+  override protected val content = getContent(path)
 
   override val tags: List[String] = getTags("Suite")
 
   override val doc: String = getDoc("Documentation")
 
   override val tests: List[Test] = {
+    def createTest(a: Int, b: Int): Test = new Test(getTest(a, b))
+
     def findTests(lines: List[String], acc: List[Int], currLine: Int): List[Int] =
       if (lines.isEmpty) acc
       else findTests(
@@ -83,8 +90,6 @@ case class Suite(private val path: String) extends Content with SuiteContent {
       case i if i.size == 1 => List(createTest(i.head, content.size))
       case _ => List(createTest(indexes.head, indexes(1))) ::: getTests(indexes.tail)
     }
-
-    def createTest(a: Int, b: Int): Test = new Test(getTest(a, b))
 
     def getTest(b: Int, e: Int): List[String] =
       if (b >= e) List()
