5b62776e3dc1d3b249ee6fc1084f6de49ab281c4
paverell <piotr.kacprzak11@gmail.com>
2015-01-26 14:55:00 +0000

    moved content to separate file

--- src/main/scala/io/jump/suite/Suite.scala
+++ src/main/scala/io/jump/suite/Suite.scala
@@ -7,37 +7,6 @@ import scala.io.Source
 // TODO create deleteWhiteSpaces method
 // TODO read and think about scala's futures
 
-
-trait ContentMatcher {
-  protected val content: List[String]
-
-  def matchFields(s: String): Option[String] = {
-    try {
-      Some(content.filter(_.contains(s)).mkString.split(":").toList.tail.head)
-    } catch {
-      case e: IndexOutOfBoundsException => None
-      case e: NoSuchElementException => None
-    }
-  }
-
-  def getTags(tagsType: String): List[String] = matchFields("@" + tagsType + " tags" + ":") match {
-    case Some(i) => i.split(",").toList.map(_.replace(" ", ""))
-    case None => List()
-  }
-}
-
-//  Get or else
-abstract class Content extends ContentMatcher {
-
-  val name: String
-  val tags: List[String]
-
-  def getDoc(docType: String): String = matchFields("@" + docType + ":") match {
-    case Some(i) => if (i.head == ' ') i.tail else i
-    case None => ""
-  }
-}
-
 trait SuiteContent extends Content {
   protected val f: File
 
