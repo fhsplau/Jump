825cd38d0bbf95fe2ded93a85a1e97515666f09f
paverell <piotr.kacprzak11@gmail.com>
2015-01-26 15:17:59 +0000

    added tests for step class

---
+++ src/test/scala/io/jump/suite/StepTest.scala
@@ -0,0 +1,10 @@
+package io.jump.suite
+
+import org.junit.runner.RunWith
+import org.scalatest.FunSuite
+import org.scalatest.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class StepTest extends FunSuite {
+
+}
