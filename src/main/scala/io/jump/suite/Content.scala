c510a0d2fc41b55ea56dfacdd8a8f7c326c10720
paverell <piotr.kacprzak11@gmail.com>
2015-01-30 14:25:43 +0000

    code refactored

--- src/main/scala/io/jump/suite/Content.scala
+++ src/main/scala/io/jump/suite/Content.scala
@@ -1,32 +1,35 @@
 package io.jump.suite
 
 sealed trait Common {
-  val FIELD_POINTER = "@"
-  val FIELD_SEPARATOR = ":"
-  val TAG_SEPARATOR = ","
   val WHITE_SPACE = " "
   val EMPTY_STRING = ""
 }
 
-sealed trait Tags extends Common {
+sealed trait Field {
+  val FIELD_POINTER = "@"
+  val FIELD_SEPARATOR = ":"
+}
 
+sealed trait Tags extends Field {
   private val TAG_FIELD_END = " tags" + FIELD_SEPARATOR
-
+  val TAG_SEPARATOR = ","
   val SUITE_TAG = FIELD_POINTER + "Suite" + TAG_FIELD_END
   val TEST_TAG = FIELD_POINTER + "Test" + TAG_FIELD_END
 }
 
-sealed trait Doc extends Common {
+sealed trait Doc extends Field {
   val SUITE_DOC = FIELD_POINTER + "Documentation"
   val TEST_DOC = FIELD_POINTER + "Test scenario"
 }
 
-sealed trait TestScenario extends Common {
+sealed trait TestScenario extends Field {
   val TEST_SCENARIO = FIELD_POINTER + "Test scenario:"
 }
 
-sealed trait ContentMatcher extends Common{
+sealed trait ContentMatcher extends Common {
   protected val content: List[String]
+  val FIELD_SEPARATOR: String
+  val TAG_SEPARATOR: String
 
   def matchFields(s: String): Option[String] = {
     try {
@@ -44,7 +47,7 @@ sealed trait ContentMatcher extends Common{
 }
 
 //  Get or else
-abstract class Content extends ContentMatcher with Tags with Doc with TestScenario{
+abstract class Content extends ContentMatcher with Tags with Doc with TestScenario {
 
   val name: String
   val tags: List[String]
